68c1fe7d86857cd3a897532f793b8e13421ed8e0
chwebdev815 <xandokan815@gmail.com>
2022-01-14 04:25:14 +0800

    added norway & english package for user friendly msg

--- upload/catalog/language/en-gb/extension/payment/two.php
+++ upload/catalog/language/en-gb/extension/payment/two.php
@@ -1,20 +1,40 @@
 <?php
 // Text
-$_['text_two_address'] = 'Two Payment';
-$_['text_business'] = 'Business';
-$_['text_personal'] = 'Personal';
+$_['text_two_address'] 	= 'Two Payment';
+$_['text_business'] 	= 'Business';
+$_['text_personal'] 	= 'Personal';
 $_['text_autocomplete'] = 'Autocomplete';
 
-$_['text_verify'] = 'By completing the purchase, you verify that you have the legal right to purchase on behalf of <strong>%s</strong>.';
-$_['text_unverify'] = '%s or <strong>Select another payment method to proceed checkout.</strong>.';
+$_['text_verify'] 		= 'By completing the purchase, you verify that you have the legal right to purchase on behalf of <strong>%s</strong>.';
+$_['text_unverify']		= '%s or <strong>Select another payment method to proceed checkout.</strong>.';
 
 // Entry
-$_['entry_account_type'] = 'Account Type <small>(Select Business for Two)</small>';
-$_['entry_company_id'] = 'Company Id';
+$_['entry_account_type']= 'Account Type <small>(Select Business for Two)</small>';
+$_['entry_company_id'] 	= 'Company Number';
 
 // Error
-$_['error_company'] = 'Your Company organization number is not valid. Please check your address.';
-$_['error_telephone'] = 'Please use phone format +47 99999999';
-$_['error_minimum'] = 'Minimum Payment using Two is 200 NOK';
-$_['error_maximum'] = 'Maximum Payment using Two is 250,000 NOK';
-$_['error_gateway'] = 'Website is not properly configured with Two payment';
+$_['error_minimum'] 	= 'Minimum Payment using Two is 200 NOK';
+$_['error_maximum'] 	= 'Maximum Payment using Two is 250,000 NOK';
+$_['error_gateway'] 	= 'Website is not properly configured with Two payment';
+
+$_['error_firstname_e'] = 'First Name is required to pay on business invoice.';
+$_['error_lastname_e']  = 'Last Name is required to pay on business invoice.';
+$_['error_email_e']		= 'Email is required to pay on business invoice.';
+$_['error_telephone_e'] = 'Telephone is required to pay on business invoice.';
+$_['error_company_e']  	= 'Company is required to pay on business invoice.';
+$_['error_city_e']  	= 'City is required to pay on business invoice.';
+$_['error_country_e']  	= 'Country is required to pay on business invoice.';
+$_['error_address_e'] = 'Address 1 is required to pay on business invoice.';
+$_['error_postcode_e']  = 'Postcode is required to pay on business invoice.';
+$_['error_organization_number_e']  = 'Company number is required to pay on business invoice.';
+
+$_['error_firstname'] = 'Please submit a valid First Name.';
+$_['error_lastname']  = 'Please submit a valid Last Name';
+$_['error_email']		= 'Please submit a valid Email.';
+$_['error_telephone'] = 'Please submit a valid Telephone.';
+$_['error_company']  	= 'Please submit a valid Company.';
+$_['error_city']  	= 'Please submit a valid City.';
+$_['error_country']  	= 'Please submit a valid Country.';
+$_['error_address'] = 'Please submit a valid Address 1.';
+$_['error_postcode']  = 'Please submit a valid Postcode.';
+$_['error_organization_number']  = 'Please submit a valid Company Number.';
